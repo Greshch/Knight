74b5800e13fecf0d47962aaaf6c3a71aa0cfd2f2
Greshch <greshchook@gmail.com>
2022-03-25 04:19:52 +0200

    board and print_board

--- src/main.cpp
+++ src/main.cpp
@@ -2,7 +2,28 @@
 #include <utility>
 using namespace std;
 
+const int ROWS = 8;
+const int COLS = 8;
+
+bool board[ROWS][COLS]
+{
+
+};
+
+void print_board()
+{
+    for (int i = 0; i < ROWS; ++i)
+    {
+        for (int j = 0; j < COLS; ++j)
+        {
+            cout << board[i][j] << " ";
+        }
+        cout << endl;
+    }
+}
+
 int main()
 {
+    print_board();
     return 0;
 }
